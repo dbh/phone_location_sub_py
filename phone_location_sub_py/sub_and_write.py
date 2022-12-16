b40a88d46367e35e7e62facdc626bacfabf60762
David Harrison <david@davidbharrison.com>
2022-12-16 13:50:46 -0700

    cleanup

--- phone_location_sub_py/sub_and_write.py
+++ phone_location_sub_py/sub_and_write.py
@@ -11,15 +11,10 @@ from model import PhoneGeo
 
 load_dotenv()
 
-# The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
     client.subscribe(mqtt_topic)
 
-# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
@@ -57,11 +52,6 @@ print(f'db_conn: {db_conn}')
 
 client.username_pw_set(os.getenv('MQTT_USER'),os.getenv('MQTT_PASSWORD'))
 client.connect(os.getenv('MQTT_SERVER'), 1883, 60)
-
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
 
 print('Create database connection')
 engine = create_engine(db_conn, pool_recycle=3600)
